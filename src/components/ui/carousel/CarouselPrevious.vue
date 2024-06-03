f5dbfd2069251de8e7cff017c5228354f496b9fa
Efekan Akbaş <efekanakbas98@gmail.com>
2024-06-03 10:55:11 +0300

    misiSEOupdate

--- src/components/ui/carousel/CarouselPrevious.vue
+++ src/components/ui/carousel/CarouselPrevious.vue
@@ -13,18 +13,20 @@ const { orientation, canScrollPrev, scrollPrev } = useCarousel()
 <template>
   <Button
     :disabled="!canScrollPrev"
-    :class="cn(
-      'touch-manipulation absolute h-8 w-8 rounded-full p-0',
-      orientation === 'horizontal'
-        ? '-left-12 top-1/2 -translate-y-1/2'
-        : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
-      props.class,
-    )"
+    :class="
+      cn(
+        'touch-manipulation absolute h-8 w-8 rounded-full p-0',
+        orientation === 'horizontal'
+          ? '-left-12 top-1/2 -translate-y-1/2'
+          : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class
+      )
+    "
     variant="outline"
     @click="scrollPrev"
   >
     <slot>
-      <ArrowLeft class="h-4 w-4 text-current" />
+      <ArrowLeft alt="previous button" class="h-4 w-4 text-current" />
     </slot>
   </Button>
 </template>
